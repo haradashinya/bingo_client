4f5b785816ddd5d2e088a3806e855bab08053093
okamurayasuyuki <haradashinya@gmail.com>
2012-12-19 11:40:31 +0900

    fixed scroll bug

--- CustomTextField.m
+++ CustomTextField.m
@@ -17,28 +17,28 @@
 {
     
     if (!_inputAccessoryView){
-        CGRect frame = CGRectMake(0, 0, 100, 44);
+        CGRect frame = CGRectMake(5, 0,480,44);
         _inputAccessoryView = [[UIView alloc] initWithFrame:frame];
         _inputAccessoryView.backgroundColor = [UIColor clearColor];
-        scrollView =[[UIScrollView alloc]initWithFrame:_inputAccessoryView.frame];
-        [scrollView setContentSize:CGSizeMake(480, 44)];
-        scrollView.scrollEnabled = YES;
-        
-        scrollView.layer.borderColor = [UIColor lightGrayColor].CGColor;
-        scrollView.layer.borderWidth = 5.0f;
-        [scrollView addSubview:_inputAccessoryView];
         
         
         
         
         __block int i = 0;
-        [@[@"x",@"sin",@"cos",@"tan",@"exp",@"^",@"logn()"] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [@[@"x",@"sin",@"cos",@"tan",@"exp",@"^",@"logn()",@"hello"] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
             
             [self addButtonWithTitle:obj with:CGRectMake(44*i, 0, 44, 44)];
             i++;
         }];
     }
-//    return _inputAccessoryView;
+    
+    scrollView =[[UIScrollView alloc]initWithFrame:_inputAccessoryView.frame];
+    [scrollView setContentSize:CGSizeMake(480, 44)];
+    scrollView.scrollEnabled = YES;
+    scrollView.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    scrollView.layer.borderWidth = 5.0f;
+    [scrollView addSubview:_inputAccessoryView];
+    //    return _inputAccessoryView;
     return scrollView;
     
 }
@@ -50,7 +50,7 @@
         [compButton setTitle:title forState:UIControlStateNormal];
         compButton.frame = frame;
         [compButton addTarget:self.delegate action:@selector(tappedChar:) forControlEvents:UIControlEventTouchUpInside];
-    [_inputAccessoryView addSubview:compButton];
+        [_inputAccessoryView addSubview:compButton];
     
     
 }
