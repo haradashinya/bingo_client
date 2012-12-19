7810bfbb8e09bd249e50738f651c95346059a830
okamurayasuyuki <haradashinya@gmail.com>
2012-12-19 10:51:40 +0900

    add border color

--- CustomTextField.m
+++ CustomTextField.m
@@ -17,6 +17,8 @@
         CGRect frame = CGRectMake(0, 0, 100, 44);
         _inputAccessoryView = [[UIView alloc] initWithFrame:frame];
         _inputAccessoryView.backgroundColor = [UIColor clearColor];
+        _inputAccessoryView.layer.borderColor = [UIColor lightGrayColor].CGColor;
+        _inputAccessoryView.layer.borderWidth = 5.0f;
         __block int i = 0;
         [@[@"x",@"sin",@"cos",@"tan",@"exp",@"^",@"logn()"] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
             
@@ -35,7 +37,6 @@
         compButton.frame = frame;
         [compButton addTarget:self.delegate action:@selector(tappedChar:) forControlEvents:UIControlEventTouchUpInside];
     [_inputAccessoryView addSubview:compButton];
-    [self.delegate hello];
     
 }
 
